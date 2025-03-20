1641a1143294f1b53b4ec0a5f4658c580eeabe86
purvaakshita <purvaakshita@gmail.com>
2025-03-20 12:06:18 +0530

    first commit

---
+++ src/components/App1.jsx
@@ -0,0 +1,8 @@
+export default function App1(){
+    return (
+        <div>
+        <h3>this is app</h3>
+        <p style={{backgroundColor:'pink',color:'blue'}}>this is paragraph</p>
+        </div>
+    );
+}
