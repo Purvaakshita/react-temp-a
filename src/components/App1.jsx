35ce13d95222f896fa083904fa386f6942b062fd
purvaakshita <purvaakshita@gmail.com>
2025-03-27 14:00:44 +0530

    updated

--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,8 +1,19 @@
-export default function App1(){
-    return (
-        <div>
-        <h3>this is app</h3>
-        <p style={{backgroundColor:'pink',color:'blue'}}>this is paragraph</p>
-        </div>
-    );
+import "./App1.css";
+import styles from "./App1.module.css"
+export default function App1() {
+  const mystyle = {
+    backgroundColor: "orange",
+    color: "blue",
+  };
+  return (
+    <div>
+      <h3>This is App1</h3>
+      <p style={{ backgroundColor: "pink", color: "blue" }}>
+        This is a paragraph
+      </p>
+      <p style={mystyle}>This is second paragraph</p>
+      <p className="AppTxt">This is third paragraph</p>
+    <p className={styles.txtPara}>This is fourth paragraph</p>
+    </div>
+  );
 }
