bfa4613f52b3fe0812353e6d6c89e1f19f78ea10
purvaakshita <purvaakshita@gmail.com>
2025-04-08 09:57:25 +0530

    added

---
+++ src/components/App20.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+export default class App20 extends Component {
+    render(){
+        return(
+            <div>
+                <h1>This is App20</h1>
+                <p>It is a class component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/App21.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+export default class App20 extends Component {
+    render(){
+        return(
+            <div>
+                <h1>This is App21</h1>
+                
+            </div>
+        )
+    }
+}
