f4b115080b7c614f998e195d9ef89a8a2f3775a3
purvaakshita <purvaakshita@gmail.com>
2025-04-10 13:49:05 +0530

    added

---
+++ src/components/App22.jsx
@@ -0,0 +1,12 @@
+// App22.jsx
+import React from "react";
+
+function App22({ name = "Guest", color = "blue" }) {
+    return (
+      <div style={{ color }}>
+        <h2>Hello, {name}!</h2>
+      </div>
+    );
+  }
+  
+  export default App22;
